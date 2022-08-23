5ef00acf11627b16f1ee0bdb4b09d1f7a3dd1b75
Ali Raza Shaheb <ali@evilrattechnologies.com>
2022-08-23 17:49:02 +0530

    fixes

--- components/generals/NavLinks.tsx
+++ components/generals/NavLinks.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { navlinks, ctaData } from "../../modules/navlinks";
-import Cta from "./Cta";
+// import Cta from "./Cta";
 import LinkComp from "./LinkComp";
 
 interface NavLinksProps {
@@ -19,11 +19,12 @@ const NavLinks = ({ className }: NavLinksProps) => {
             </Link>
           );
         })}
-        <Cta
+
+        {/* <Cta
           text={ctaData.quote.text}
           icon={`/icons/long_aero.svg`}
           className="border-none outline-none bg-baOrange text-baWhite p-3 px-4 ml-6 flex justify-center items-center"
-        />
+        /> */}
       </div>
     </>
   );
